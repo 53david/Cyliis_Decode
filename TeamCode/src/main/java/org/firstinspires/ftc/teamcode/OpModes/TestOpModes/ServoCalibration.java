b2ddd38253288930627fda504c4ac66289190113
Liteanu David <davidliteanu.oficial@gmail.com>
2026-06-09 09:30:28 +0300

    yara yara phonk

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TestOpModes/ServoCalibration.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TestOpModes/ServoCalibration.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.OpModes.TestOpModes;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.PwmControl;
 
 import org.firstinspires.ftc.teamcode.Wrappers.Initializer;
 
@@ -10,6 +11,8 @@ public class ServoCalibration extends LinearOpMode {
     @Override
     public void runOpMode(){
         Initializer.start(hardwareMap);
+        Initializer.servo1.setPwmRange(new PwmControl.PwmRange(500,2500));
+        Initializer.servo2.setPwmRange(new PwmControl.PwmRange(500,2500));
         waitForStart();
         while (opModeIsActive()){
             Initializer.servo1.setPosition(0.5);
