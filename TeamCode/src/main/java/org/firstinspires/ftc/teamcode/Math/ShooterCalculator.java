12f7152ca89445558fd2faaa4e66cd8ccc24f4aa
Liteanu David <davidliteanu.oficial@gmail.com>
2026-06-26 22:44:29 +0300

    Sper ca merge - sotm 👀

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Math/ShooterCalculator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Math/ShooterCalculator.java
@@ -16,12 +16,7 @@ public class ShooterCalculator {
     public static double regression = 0.00045;
     public static double g = 9810.0;
     public static double goalHeight = 67.5;
-    public static double passThroughAngle = -Math.PI / 6;
-    public static double maxHoodAngle = Math.PI / 3;
-    public static double minHoodAngle = Math.PI / 6;
-    public static double maxHoodPos = 0.496;
-    public static double minHoodPos = 0.11;
-
+    public static double theta = -Math.PI / 6;
     public static double targetAngle = 0;
     public static double hoodPos = 0.5;
     public static double vel = 1200;
@@ -30,35 +25,31 @@ public class ShooterCalculator {
         double distance = Math.hypot(goalX - robotX, goalY - robotY);
         double gAngle = Math.atan2(goalY - robotY, goalX - robotX);
 
-        double alpha = Math.atan((2 * goalHeight / distance) - Math.tan(passThroughAngle));
-        double v0 = Math.sqrt(Math.abs((g * Math.pow(distance, 2)) / (2 * Math.pow(Math.cos(alpha), 2)
-                * (distance * Math.tan(alpha) - goalHeight))));
+        double alpha = Math.atan((2 * goalHeight / distance) - Math.tan(theta));
+        double v0 = Math.sqrt(Math.abs((g * Math.pow(distance, 2)) / (2 * Math.pow(Math.cos(alpha), 2) * (distance * Math.tan(alpha) - goalHeight))));
 
-        double vAngle = Math.atan2(velY, velX);
-
-        double rAngle = vAngle - gAngle;
-        double vRadial = -Math.cos(rAngle) * Odo.avgVel();
-        double vTan = Math.sin(rAngle) * Odo.avgVel();
+        double rAngle = Math.atan2(velY, velX) - gAngle;
+        double vParallel = -Math.cos(rAngle) * Odo.avgVel();
+        double vPerpendicular = Math.sin(rAngle) * Odo.avgVel();
 
         double time = distance / (v0 * Math.cos(alpha));
 
-        double vCompensation = (distance / time) + vRadial;
-        double vx = Math.hypot(vCompensation, vTan);
+        double vCompensation = (distance / time) + vParallel;
+        double vx = Math.hypot(vCompensation, vPerpendicular);
         double vy = v0 * Math.sin(alpha);
 
         double newAlpha = Math.atan2(vy, vx);
         double velocity = Math.hypot(vx, vy);
 
-        double turretOffset = Math.atan2(vTan, vCompensation);
+        double turretOffset = Math.atan2(vPerpendicular, vCompensation);
 
         targetAngle = gAngle + turretOffset;
-        hoodPos = FromRadsToPos(Math.clamp(newAlpha, minHoodAngle, maxHoodAngle));
+        hoodPos = FromRadsToPos(Math.clamp(newAlpha, Math.PI/6, Math.PI/3));
         vel = FromVelocityToTicks(velocity);
     }
 
     private static double FromRadsToPos(double angle) {
-        double pos = ((minHoodPos - maxHoodPos) / (minHoodAngle - maxHoodAngle)) * (angle - minHoodAngle) + minHoodPos;
-        return Math.clamp(pos, minHoodPos, maxHoodPos);
+        return Math.clamp(0.742308 * angle-0.276,0.11,0.496);
     }
 
     private static double FromVelocityToTicks(double velocity) {
